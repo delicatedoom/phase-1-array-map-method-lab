1a64945f4dbf5a5b3366a41a3a15d95add3f5bb8
JacquelineMalatare <jacqueline.malatare@gmail.com>
2022-06-15 08:49:12 -0600

    commit tree d160a411828839a34001a4924d3dd34f37a2cc2a

--- index.js
+++ index.js
@@ -11,6 +11,31 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials
+// titleCased(tutorials)
+// console.log(tutorials)
+
+//const titleCased = tutorials.map(title => title.charAt(0).toUpperCase() + title.substr(1).toLowerCase())
+
+// const titleCased = () => {
+//   return tutorials
+// }
+
+// words.map((word) => {
+    //   return word[0].toUpperCase() + word.substring(1)
+    // }).join(" ")
+
+// const titleCased = () => {
+//   let newTutorials = tutorials.map(function (element) {
+//     const words = element.split(" ")
+//     return words.map(word => word.charAt(0).toUpperCase() + word.substring(1)).join(" ")
+//   })
+//   return newTutorials
+// }
+
+function titleCased(){
+  let mappedArray = tutorials.map(function (element) {
+    const words = element.split(" ")
+    return words.map(word => word[0].toUpperCase() + word.substring(1)).join(" ")
+  })
+  return mappedArray
 }
